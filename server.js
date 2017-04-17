43edd320565011bc64021cd85ddcd627a91bab6a
Ciampije <josephciampi@C1MRQ06AHF4H.fios-router.home>
2017-04-17 17:04:56 -0400

    updating package.json and updating code for heroku

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
+var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/basketball_app';
+port = process.env.PORT || 3000;
+
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended:false}));
@@ -21,12 +24,13 @@ app.get('/', function(req, res){
 	res.render('index.ejs');
 });
 
-mongoose.connect('mongodb://localhost:27017/project2');
+mongoose.connect(mongoUri);
 
 mongoose.connection.once('open', function(){
 	console.log('connected to mongo');
 });
 
-app.listen(3000, function(){
-	console.log('listening....');
-});
+app.listen(port);
+console.log('---------------------------------');
+console.log('Server running on port: ' + port);
+console.log('---------------------------------');
